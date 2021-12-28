8e98d42fed7ed49e164e63ba4499a5b078719dab
Shmiel Minz <s0583223395@gmail.com>
2021-12-28 15:50:56 +0000

    #2 needs correction

--- 2 Table.sql
+++ 2 Table.sql
@@ -1,9 +1,11 @@
+-- SM Great work 98% see comments below and resubmit
 use AirlineDB
 go 
 drop table if exists Flight
 go
 create table dbo.Flight(
     FlightID int not null identity primary key,
+-- SM on all char add constraint that it cant be less
     FlightNumber char (6) not null 
         constraint ck_Flight_FlightNumber_cannot_be_blank check(FlightNumber <> ''),
     DepartureAirport char(3) not null 
@@ -14,6 +16,7 @@ create table dbo.Flight(
         constraint ck_Flight_ArrivalAirport_cannot_be_blank check(ArrivalAirport <> ''),
     ArrivalCountry varchar(25) not null
         constraint ck_Flight_ArrivalCountry_cannot_be_blank check(ArrivalCountry <> ''),
+-- SM Use 1 column for date and time, it would help for constraint that departure can't be after arrival
     DateDeparting date not null,    
     TimeDeparting Varchar(15) not null,
     DateArriving date not null,
@@ -23,19 +26,23 @@ create table dbo.Flight(
     PassengerLastName varchar(25) not null
         constraint ck_Flight_PassengerLastName_cannot_be_blank check(PassengerLastName <> ''),
     DateOfBirth date not null
+-- SM this should be date of travel not todays date
         constraint ck_Passenger_must_be_between_16_and_90_years_old check(datediff(year,DateOfBirth,Getdate()) between 16 and 90),
     PassengerAddress varchar(30) not null
         constraint ck_Flight_PassengerAddress_cannot_be_blank check(PassengerAddress <> ''),
+-- SM should be as char()/varchar() with constraint that needs to be numeric and can't start with 0, so there shouldn't be 0 in reports.
     PassportNumber int not null
         constraint ck_PassportNumber_must_be_a_9_digits_number check(len(PassportNumber) = 9 or PassportNumber = ''),
     DateOfIssue Date null,
     DateOfExpiry date null
+-- SM this should be date of travel not todays date
         constraint ck_Flight_DateOfExpiry_must_be_valid check(DateOfExpiry >= getdate()),
     Nationality varchar(25) not null,
     constraint u_FlightNumber_and_PassengerName_and_DateOfBirth_and_TimeDeparting unique(FlightNumber,PassengerFirstName,PassengerLastName,DateOfBirth,TimeDeparting),
     constraint ck_Flight_DateDeparting_cannot_be_After_DateArriving check(DateDeparting <= DateArriving),
     constraint ck_PassportNumber_and_DateOfIssueand_DateOfExpiry_and_DateOfExpiry_and_Nationality_must_all_have_a_value_or_all_be_blank
         check((PassportNumber = '' and DateOfIssue is null and DateOfExpiry is null and Nationality = '') or (PassportNumber <> '' and DateOfIssue is not null and DateOfExpiry is not null and Nationality <> '')),
+-- SM sorry for making this to complicated but some of the getdate() should be issue date and some date of travel
     constraint ck_Flight_DateOfIssue_must_be_according_to_the_rules 
         check(
            (DepartureCountry = ArrivalCountry and DateOfExpiry >= getdate()) 
