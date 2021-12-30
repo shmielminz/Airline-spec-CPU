05ae0095a407ceeff9a6f29d55a04e1857e309b3
Mimi Levy <264mimi@gmail.com>
2021-12-30 12:51:20 +0200

    Fixed work on Airline Scenario

--- 2 Table.sql
+++ 2 Table.sql
@@ -5,43 +5,41 @@ go
 create table dbo.Flight(
     FlightID int not null identity primary key,
     FlightNumber char (6) not null 
-        constraint ck_Flight_FlightNumber_cannot_be_blank check(FlightNumber <> ''),
+        constraint ck_Flight_FlightNumber_must_have_6_characters check(len(FlightNumber) = 6),
     DepartureAirport char(3) not null 
-        constraint ck_Flight_DepartureAirport_cannot_be_blank check(DepartureAirport <> ''),
+        constraint ck_Flight_DepartureAirport_must_have_3_characters check(len(DepartureAirport) = 3),
     DepartureCountry varchar(25) not null
         constraint ck_Flight_DepartureCountry_cannot_be_blank check(DepartureCountry <> ''),
     ArrivalAirport char(3) not null
-        constraint ck_Flight_ArrivalAirport_cannot_be_blank check(ArrivalAirport <> ''),
+        constraint ck_Flight_ArrivalAirport_must_have_3_characters check(len(ArrivalAirport) = 3),
     ArrivalCountry varchar(25) not null
         constraint ck_Flight_ArrivalCountry_cannot_be_blank check(ArrivalCountry <> ''),
-    DateDeparting date not null,    
-    TimeDeparting Varchar(15) not null,
-    DateArriving date not null,
-    TimeArriving varchar(15) not null,
+    DateAndTimeDeparting datetime not null,    
+    DateAndTimeArriving datetime not null,
     PassengerFirstName varchar(25) not null
         constraint ck_Flight_PassengerFirstName_cannot_be_blank check(PassengerFirstName <> ''),
     PassengerLastName varchar(25) not null
         constraint ck_Flight_PassengerLastName_cannot_be_blank check(PassengerLastName <> ''),
-    DateOfBirth date not null
-        constraint ck_Passenger_must_be_between_16_and_90_years_old check(datediff(year,DateOfBirth,Getdate()) between 16 and 90),
+    DateOfBirth date not null,
     PassengerAddress varchar(30) not null
         constraint ck_Flight_PassengerAddress_cannot_be_blank check(PassengerAddress <> ''),
     PassportNumber int not null
         constraint ck_PassportNumber_must_be_a_9_digits_number check(len(PassportNumber) = 9 or PassportNumber = ''),
     DateOfIssue Date null,
-    DateOfExpiry date null
-        constraint ck_Flight_DateOfExpiry_must_be_valid check(DateOfExpiry >= getdate()),
+    DateOfExpiry date null,
     Nationality varchar(25) not null,
-    constraint u_FlightNumber_and_PassengerName_and_DateOfBirth_and_TimeDeparting unique(FlightNumber,PassengerFirstName,PassengerLastName,DateOfBirth,TimeDeparting),
-    constraint ck_Flight_DateDeparting_cannot_be_After_DateArriving check(DateDeparting <= DateArriving),
+    constraint u_FlightNumber_and_PassengerName_and_DateOfBirth_and_TimeDeparting unique(FlightNumber,PassengerFirstName,PassengerLastName,DateOfBirth,DateAndTimeDeparting),
+    constraint ck_Flight_DateAndTimeDeparting_cannot_be_After_DateAndTimeArriving check(convert(date,DateAndTimeDeparting,111) <= convert(date,DateAndTimeArriving,111)),
+    constraint ck_Passenger_must_be_between_16_and_90_years_old check(datediff(year,DateOfBirth,convert(date,DateAndTimeDeparting,111)) between 16 and 90),
+    constraint ck_Flight_DateOfExpiry_must_be_valid check(DateOfExpiry >= convert(date,DateAndTimeDeparting,111)),
     constraint ck_PassportNumber_and_DateOfIssueand_DateOfExpiry_and_DateOfExpiry_and_Nationality_must_all_have_a_value_or_all_be_blank
         check((PassportNumber = '' and DateOfIssue is null and DateOfExpiry is null and Nationality = '') or (PassportNumber <> '' and DateOfIssue is not null and DateOfExpiry is not null and Nationality <> '')),
     constraint ck_Flight_DateOfIssue_must_be_according_to_the_rules 
         check(
-           (DepartureCountry = ArrivalCountry and DateOfExpiry >= getdate()) 
-        or (ArrivalCountry = Nationality and DateOfExpiry >= getdate())
-        or ((DepartureCountry <> ArrivalCountry or ArrivalCountry <> Nationality or datediff(year,DateOfBirth,getdate()) >= 16) and datediff(year,DateOfIssue,getdate()) <= 9.5)
-        or (datediff(year,DateOfBirth,getdate()) < 16 and datediff(year,DateOfIssue,getdate()) <= 5) 
+           (DepartureCountry = ArrivalCountry and DateOfExpiry >= convert(date,DateAndTimeDeparting,111)) 
+        or (ArrivalCountry = Nationality and DateOfExpiry >= convert(date,DateAndTimeDeparting,111))
+        or ((DepartureCountry <> ArrivalCountry or ArrivalCountry <> Nationality or datediff(year,DateOfBirth,convert(date,DateAndTimeDeparting,111)) >= 16) and datediff(year,DateOfIssue,convert(date,DateAndTimeDeparting,111)) <= 9.5)
+        or (datediff(year,DateOfBirth,DateOfIssue) < 16 and datediff(year,DateOfIssue,convert(date,DateAndTimeDeparting,111)) <= 5) 
              )
 )
 go 
--- 5 Reports.sql
+++ 5 Reports.sql
@@ -3,19 +3,19 @@ go
 
 --The reports I need is as following:
     --1) How many people booked per flight, as flight number, departure airport, when the flight is departing, destination.
-select BookedPeopleOnFlight = count(*), FlightNumber, DepartureAirport, TimeDeparting, ArrivalCountry
+select BookedPeopleOnFlight = count(*), FlightNumber, DepartureAirport, DateAndTimeDeparting, ArrivalCountry
 from flight 
-group by FlightNumber, DepartureAirport, TimeDeparting, ArrivalCountry
+group by FlightNumber, DepartureAirport, DateAndTimeDeparting, ArrivalCountry
 
     --2) Who isn't checked in for flights departing in the next week, in order to send them reminders to check in.
 select *
 from Flight f
-where f.DateDeparting between getdate() and dateadd(day,7,getdate())
+where convert(date,DateAndTimeDeparting,111) between getdate() and dateadd(day,7,getdate())
 
     --3) How many flights are departing per day, and num of passengers we have on those flights.
-select NumOfFlights = count(*), f.DateDeparting, NumOfPassengers = count(f.PassengerLastName)
+select NumOfFlights = count(*), convert(date,DateAndTimeDeparting,111), NumOfPassengers = count(f.PassengerLastName)
 from Flight f
-group by DateDeparting
+group by convert(date,DateAndTimeDeparting,111)
 
     --4) How many flights are departing per destination, and num of passengers we have on those flights, to know what route is attracts most people.
 select NumOfFlight = count(*), ArrivalCountry, NumOfPassengers = count(f.PassengerLastName)
@@ -25,7 +25,7 @@ group by ArrivalCountry
     --5) How many people booked but in the end they didn't travel.
 select *
 from Flight f 
-where f.DateDeparting <= getdate()
+where convert(date,DateAndTimeDeparting,111) <= getdate()
 and f.PassportNumber = ''
 
     --6) How many flights does each person have (to know for frequent flyer status), as last name, first name, number of flights.
